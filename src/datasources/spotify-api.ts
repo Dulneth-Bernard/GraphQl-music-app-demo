5b79e38957f966b0692c69903799f9609bd14b5d
Your Name <dulneth.bernard@gmail.com>
2024-05-21 14:55:47 +0530

    setting up the datsource for grapghQl, (Rest Api)

---
+++ src/datasources/spotify-api.ts
@@ -0,0 +1,14 @@
+import { RESTDataSource } from "@apollo/datasource-rest";
+
+export class SpotifyAPI extends RESTDataSource {
+  
+  baseURL = "https://spotify-demo-api-fe224840a08c.herokuapp.com/v1/";
+  
+  async getFeaturedPlaylists() {
+    const response = await this.get<{ playlists: { items: any[] }}>("browse/featured-playlists");
+    return response?.playlists?.items ?? [];
+  }
+
+}
+
+//Because it's a very common task to fetch data from REST when building a GraphQL API, Apollo Server provides a dedicated DataSource class for just that: the RESTDataSource.
